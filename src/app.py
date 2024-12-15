5b1a5545476b046d9ec073943d9538e9a3311194
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-12-15 20:38:08 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Posts
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,42 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def handle_all():
+    users = User.query.all()
+    users = [user.serialize() for user in users]
+    return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route('/posts', methods=['GET'])
+def handle_get_posts():
+    posts = Posts.query.all()
+    posts = [post.serialize() for post in posts]
+    return jsonify(posts), 200
+
+
+@app.route('/user/<int:id>', methods=['GET'])
+def handle_one(id):
+    user = User.query.get(id)
+    respObj = {
+        "success" : True,
+        "data" : user.serialize()
     }
+    return jsonify(respObj), 200
 
-    return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    data = request.json
+    new_user = User(
+        email = data["email"],
+        password = data["password"],
+        city = data["city"],
+        country = data["country"],
+        is_active = data["is_active"]
+        )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,49 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+## cuando se modifica la tabla -> pipenv run migrate -> pipenv run upgrade/downgrade
+
 class User(db.Model):
+    __tablename__= 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    city = db.Column(db.String(150))
+    country = db.Column(db.String(150), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    posts = db.relationship('Posts', backref='user', lazy=True)
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "city" : self.city,
+            "is_active" : self.is_active
             # do not serialize the password, its a security breach
         }
+    
+class Posts(db.Model):
+    __tablename__= 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=True, nullable=False)
+    content = db.Column(db.String(250), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
+    def __repr__(self):
+        return '<Posts %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content" : self.content,
+            "user_id" : self.user_id
+        }
+    
+class Group
